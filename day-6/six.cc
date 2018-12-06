0e7b3231bd68d34cd1bc21911ba6730f2b991ff2
Joshua Borkowski-Clark <jabc@google.com>
2018-12-06 11:07:31 +0100

    Day six part two

--- day-6/six.cc
+++ day-6/six.cc
@@ -1,7 +1,10 @@
+#include <algorithm>
 #include <fstream>
+#include <functional>
 #include <iostream>
 #include <limits>
 #include <map>
+#include <numeric>
 #include <regex>
 #include <set>
 #include <string>
@@ -10,7 +13,12 @@
 
 using namespace std;
 
-vector<pair<int, int>> readSequence() {
+struct Puzzle {
+  vector<pair<int, int>> points;
+  pair<int, int> bounds;
+};
+
+Puzzle readSequence() {
   string line;
   std::ifstream myfile("6.input");
   vector<pair<int, int>> lines;
@@ -30,8 +38,10 @@ vector<pair<int, int>> readSequence() {
       lines.push_back(make_pair(first, second));
     }
   }
-  lines.push_back(make_pair(maxX + 1, maxY + 1));
-  return lines;
+  struct Puzzle puzzle;
+  puzzle.bounds = make_pair(maxX + 1, maxY + 1);
+  puzzle.points = lines;
+  return puzzle;
 }
 
 int manhattan(pair<int, int> a, pair<int, int> b) {
@@ -50,9 +60,8 @@ int minId(pair<int, int> a, vector<pair<int, int>> positions) {
   return minDistPoints.size() == 1 ? *minDistPoints.begin() : -1;
 }
 
-void partOne(vector<pair<int, int>> puzzle) {
-  pair<int, int> maxSize = puzzle.back();
-  puzzle.pop_back();
+void partOne(Puzzle puzzle) {
+  pair<int, int> maxSize = puzzle.bounds;
   const int maxX = maxSize.first;
   const int maxY = maxSize.second;
 
@@ -62,14 +71,14 @@ void partOne(vector<pair<int, int>> puzzle) {
   }
 
   int id = 1;
-  for (auto pair : puzzle) {
+  for (auto pair : puzzle.points) {
     array[pair.first][pair.second] = id;
     id++;
   }
 
   for (int x = 0; x < maxX; x++) {
     for (int y = 0; y < maxY; y++) {
-      array[x][y] = minId(make_pair(x, y), puzzle);
+      array[x][y] = minId(make_pair(x, y), puzzle.points);
     }
   }
 
@@ -98,9 +107,29 @@ void partOne(vector<pair<int, int>> puzzle) {
   cout << "Biggest area: " << biggestSize;
 }
 
-void partTwo(vector<pair<int, int>> puzzle) {
-  pair<int, int> maxSize = puzzle.back();
-  puzzle.pop_back();
+int totalDistance(pair<int, int> p, vector<pair<int, int>> locs) {
+  vector<int> distances;
+  std::transform(locs.begin(), locs.end(), back_inserter(distances),
+                 [&p](pair<int, int> l) -> int { return manhattan(l, p); });
+  return std::accumulate(distances.begin(), distances.end(), 0);
+}
+
+void partTwo(Puzzle puzzle) {
+  pair<int, int> maxSize = puzzle.bounds;
+  const int maxX = maxSize.first;
+  const int maxY = maxSize.second;
+
+  int size = 0;
+
+  for (int x = 0; x < maxX; x++) {
+    for (int y = 0; y < maxY; y++) {
+      if (totalDistance(make_pair(x, y), puzzle.points) < 10000) {
+        size++;
+      }
+    }
+  }
+
+  cout << "Total size of area: " << size << "\n";
 }
 
 int main() {
