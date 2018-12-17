181b8c089cb129b7ed56b027355da590efd327e4
Joshua Clark <joshua@clark.io>
2018-12-17 21:15:37 +0100

    Day 14
    Part 1 passed
    Part 2 eating memory

---
+++ day-14/fourteen.cc
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <list>
+#include <vector>
+
+void step(std::list<short> *input, std::list<short>::iterator &elfOne,
+          std::list<short>::iterator &elfTwo) {
+  int sum = *elfOne + *elfTwo;
+  int oneAdvance = 1 + *elfOne;
+  int twoAdvance = 1 + *elfTwo;
+
+  if (sum >= 10) {
+    input->insert(input->end(), sum / 10);
+  }
+  input->insert(input->end(), sum % 10);
+
+  while (oneAdvance > 0 ) {
+    elfOne++; oneAdvance--;
+    if (elfOne == input->end()) {
+      elfOne = input->begin();
+    }
+  }
+
+  while (twoAdvance > 0 ) {
+    elfTwo++; twoAdvance--;
+    if (elfTwo == input->end()) {
+      elfTwo = input->begin();
+    }
+  }
+}
+
+std::pair<int, int> step(std::vector<short> &input, int elfOne, int elfTwo) {
+  int sum = input.at(elfOne) + input.at(elfTwo);
+  if (sum >= 10) {
+    input.push_back(sum / 10);
+  }
+  input.push_back(sum % 10);
+
+  elfOne = (elfOne + 1 + input.at(elfOne)) % input.size();
+  elfTwo = (elfTwo + 1 + input.at(elfTwo)) % input.size();
+  return std::make_pair(elfOne, elfTwo);
+}
+
+void partOne(std::vector<short> input, int iterations) {
+  int elfOne = 0;
+  int elfTwo = 1;
+
+  while (input.size() < iterations + 10) {
+    auto elves = step(input, elfOne, elfTwo);
+    elfOne = elves.first;
+    elfTwo = elves.second;
+  }
+
+  int initialLength = input.size();
+
+  for (int i = iterations; i < iterations + 10; i++) std::cout << input.at(i);
+  std::cout << "\n";
+}
+
+// Returns the digits in reverse order.
+std::vector<short> digits(int value) {
+  std::vector<short> v;
+  while (value != 0) {
+    v.push_back(value % 10);
+    value /= 10;
+  }
+  return v;
+}
+
+bool endsWith(std::list<short> *sequence, std::vector<short> *target) {
+  auto seqIt = sequence->rbegin();
+  auto targIt = target->begin();
+  while (seqIt != sequence->rend() && targIt != target->end()) {
+    if (*seqIt != *targIt) {
+      return false;
+    }
+    seqIt++;
+    targIt++;
+  }
+  return target->end() == targIt;
+}
+
+template<typename T>
+std::ostream& operator<<(std::ostream& s, const std::list<T>& v) {
+    for (const auto& e : v) {
+        s << e;
+    }
+    return s << '\n';
+}
+
+
+void partTwo(std::list<short> input, int target) {
+  auto elfOne = input.begin();
+  auto elfTwo = input.begin();
+  elfTwo++;
+
+  auto targetVector = digits(target);
+  int iterations = 0;
+
+  while (!endsWith(&input, &targetVector)) {
+    step(&input, elfOne, elfTwo);
+    if (iterations < 5) {
+      std::cout << input;
+      iterations++;
+    }
+  }
+
+  std::cout << "Prior recipes: " << (input.size() - targetVector.size())
+            << "\n";
+}
+
+int main() {
+  std::vector<short> input = {3, 7};
+
+  std::list<short> inputList = {3, 7};
+
+  partOne(input, 637061);
+
+  partTwo(inputList, 51589);
+  partTwo(inputList, 92510);
+  partTwo(inputList, 59414);
+
+  partTwo(inputList, 637061);
+}
